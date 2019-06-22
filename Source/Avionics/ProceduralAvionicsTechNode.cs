e15791f68077f8fc394ca37f6d4f3f43b9fd0cc0
Mike <11480934+MikeOnTea@users.noreply.github.com>
2019-06-22 20:59:01 +0200

    Set default SAS level to 5

--- Source/Avionics/ProceduralAvionicsTechNode.cs
+++ Source/Avionics/ProceduralAvionicsTechNode.cs
@@ -99,7 +99,7 @@ namespace RP0.ProceduralAvionics
 
 		// This is the service level of the SAS part module
 		[Persistent]
-		public int SASServiceLevel = 0;
+		public int SASServiceLevel = 5;
 
 		// Controls whether or not this part has a science return container 
 		[Persistent]
