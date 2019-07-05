bc798277bc2856d77c1d6b55b5f6da18f286ade5
Mike <11480934+MikeOnTea@users.noreply.github.com>
2019-07-05 21:47:08 +0200

    Lower avionics kct multiplier to 1.5

--- Source/ModuleTag/Avionics.cs
+++ Source/ModuleTag/Avionics.cs
@@ -4,8 +4,13 @@
     {
         public override string GetInfo()
         {
+<<<<<<< master
             return "Contains avionics to control the craft which requires extensive testing which increase the overall launch cost.\n\n" +
                    "<b><color=orange>Launch Cost: Cost of This Part * 3</color></b>";
+=======
+            return "Contains avionics to control the craft which requires extensive testing which increase the overall Rollout Cost.\n\n" +
+    "<b><color=orange>Rollout Cost: Cost of This Part * 1.5</color></b>";
+>>>>>>> Lower avionics kct multiplier to 1.5
         }
     }
 }
